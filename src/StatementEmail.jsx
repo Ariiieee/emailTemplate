7a838194d86c44a10cc1461715fe61ada8124986
Ariiie <oluwafunmilayodada2018@gmail.com>
2023-09-14 12:50:16 +0100

    fix flex box not supporting html for email

--- src/StatementEmail.jsx
+++ src/StatementEmail.jsx
@@ -1,3 +1,237 @@
+// import React from "react";
+
+// const StatementEmail = () => {
+// 	return (
+// 		<>
+// 			<html>
+// 				<head>
+// 					<style>
+// 						{`@font-face {
+// 							font-family:"Gordita";
+// 							src: url('https://vccloud.blob.core.windows.net/omnipayblob/email-assets/GroditaMedium.woff')
+// 							format('woff'),
+// 							url('https://vccloud.blob.core.windows.net/omnipayblob/email-assets/GroditaRegular.woff')
+// 							format('woff');
+
+// 						}`}
+// 					</style>
+// 				</head>
+// 				<body
+// 					style={{
+// 						fontFamily: "GroditaRegular, sans-serif",
+// 						fontSize: "16px",
+// 						margin: "0",
+// 						padding: "0",
+// 						boxSizing: "border-box",
+// 						backgroundColor: "#fff",
+// 						minHeight: "100vh",
+// 					}}
+// 				>
+// 					<main
+// 						style={{
+// 							padding: "48px 24px 32px 24px",
+// 							margin: "0 auto",
+// 							maxWidth: "640px",
+// 						}}
+// 					>
+// 						<div
+// 							style={{
+// 								marginTop: "32px",
+// 								display: "flex",
+// 								gap: "20px",
+// 								justifyContent: "center",
+// 								alignItems: "center",
+// 							}}
+// 						>
+// 							<div
+// 								style={{
+// 									margin: "0px",
+// 									textAlign: "center",
+// 									padding: "24px 0px 24px 0",
+// 									objectFit: "contain",
+// 									width: "145px",
+// 								}}
+// 							>
+// 								<img
+// 									width={145}
+// 									src="https://vccloud.blob.core.windows.net/omnipayblob/email-assets/logo.png"
+// 									alt=""
+// 								/>
+// 							</div>
+// 						</div>
+// 						<div
+// 							style={{
+// 								padding: "16px",
+// 								fontSize: "16px",
+// 								lineHeight: "32px",
+// 							}}
+// 						>
+// 							<p>
+// 								Dear Customer
+// 								<br />
+// 								Your wallet account statement for selected time
+// 								period is now available (see attached).
+// 								<br />
+// 								If you did not initiate this request, please
+// 								change your PIN.
+// 								<br />
+// 								For any feedback or enquiries, please call
+// 								support on 08000900999
+// 							</p>
+// 							<p
+// 								style={{
+// 									marginTop: "12px",
+// 									fontFamily: "Gordita",
+// 									fontWeight: "500",
+// 									fontSize: "16px",
+// 									color: "#344054",
+// 								}}
+// 							>
+// 								Thank you for choosing OmniPay
+// 							</p>
+// 						</div>
+// 						<div
+// 							style={{
+// 								paddingTop: "16px",
+// 								display: "flex",
+// 								flexDirection: "column",
+// 								justifyContent: "center",
+// 								alignItems: "center",
+// 								color: "#101828",
+// 								fontWeight: "500",
+// 								lineHeight: "24px",
+// 							}}
+// 						>
+// 							<p
+// 								style={{
+// 									color: "#101828",
+// 									fontFamily: "Gordita",
+// 								}}
+// 							>
+// 								Download the app
+// 							</p>
+// 							<p
+// 								style={{
+// 									fontFamily: "Gordita",
+// 									color: "#667085",
+// 								}}
+// 							>
+// 								Get the most of OmniPay by installing our new
+// 								mobile app.
+// 							</p>
+// 						</div>
+// 						<div
+// 							style={{
+// 								marginTop: "24px",
+// 								display: "flex",
+// 								gap: "12px",
+// 								justifyContent: "center",
+// 							}}
+// 						>
+// 							<div
+// 								style={{
+// 									width: "120px",
+// 									objectFit: "contain",
+// 								}}
+// 							>
+// 								<img
+// 									src="https://vccloud.blob.core.windows.net/omnipayblob/email-assets/appstore.png"
+// 									width={120}
+// 									height={40}
+// 									alt=""
+// 								/>
+// 							</div>
+// 							<div
+// 								style={{
+// 									width: "135px",
+// 									objectFit: "contain",
+// 								}}
+// 							>
+// 								<img
+// 									width={135}
+// 									height={40}
+// 									src="https://vccloud.blob.core.windows.net/omnipayblob/email-assets/playstore.png"
+// 									alt=""
+// 								/>
+// 							</div>
+// 						</div>
+// 					</main>
+
+// 					<footer>
+// 						<div
+// 							style={{
+// 								marginTop: "32px",
+// 								display: "flex",
+// 								gap: "20px",
+// 								justifyContent: "center",
+// 								alignItems: "center",
+// 							}}
+// 						>
+// 							<div
+// 								style={{
+// 									display: "flex",
+// 									justifyContent: "center",
+// 									alignItems: "center",
+// 									width: "20px",
+// 									height: "20px",
+// 									objectFit: "contain",
+// 								}}
+// 							>
+// 								<img
+// 									src="https://vccloud.blob.core.windows.net/omnipayblob/email-assets/twitter.png"
+// 									width={20}
+// 									height={20}
+// 									alt=""
+// 								/>
+// 							</div>
+// 							<div
+// 								style={{
+// 									display: "flex",
+
+// 									justifyContent: "center",
+// 									alignItems: "center",
+// 									width: "20px",
+// 									height: "20px",
+// 									objectFit: "contain",
+// 								}}
+// 							>
+// 								<img
+// 									src="https://vccloud.blob.core.windows.net/omnipayblob/email-assets/facebook.png"
+// 									width={20}
+// 									height={20}
+// 									alt=""
+// 								/>
+// 							</div>
+// 							<div
+// 								style={{
+// 									display: "flex",
+
+// 									justifyContent: "center",
+// 									alignItems: "center",
+// 									width: "20px",
+// 									height: "20px",
+
+// 									objectFit: "contain",
+// 								}}
+// 							>
+// 								<img
+// 									src="https://vccloud.blob.core.windows.net/omnipayblob/email-assets/instagram.png"
+// 									width={20}
+// 									height={20}
+// 									alt=""
+// 								/>
+// 							</div>
+// 						</div>
+// 					</footer>
+// 				</body>
+// 			</html>
+// 		</>
+// 	);
+// };
+
+// export default StatementEmail;
+
+
 import React from "react";
 
 const StatementEmail = () => {
@@ -37,19 +271,16 @@ const StatementEmail = () => {
 						<div
 							style={{
 								marginTop: "32px",
-								display: "flex",
-								gap: "20px",
-								justifyContent: "center",
-								alignItems: "center",
+								textAlign: "center",
 							}}
 						>
 							<div
 								style={{
-									margin: "0px",
 									textAlign: "center",
 									padding: "24px 0px 24px 0",
 									objectFit: "contain",
 									width: "145px",
+									margin: "0 auto",
 								}}
 							>
 								<img
@@ -64,6 +295,7 @@ const StatementEmail = () => {
 								padding: "16px",
 								fontSize: "16px",
 								lineHeight: "32px",
+
 							}}
 						>
 							<p>
@@ -93,10 +325,7 @@ const StatementEmail = () => {
 						<div
 							style={{
 								paddingTop: "16px",
-								display: "flex",
-								flexDirection: "column",
-								justifyContent: "center",
-								alignItems: "center",
+								textAlign: "center",
 								color: "#101828",
 								fontWeight: "500",
 								lineHeight: "24px",
@@ -123,15 +352,15 @@ const StatementEmail = () => {
 						<div
 							style={{
 								marginTop: "24px",
-								display: "flex",
-								gap: "12px",
-								justifyContent: "center",
+								textAlign: "center",
 							}}
 						>
 							<div
 								style={{
 									width: "120px",
 									objectFit: "contain",
+									display: "inline-block",
+									marginRight: "12px",
 								}}
 							>
 								<img
@@ -145,6 +374,7 @@ const StatementEmail = () => {
 								style={{
 									width: "135px",
 									objectFit: "contain",
+									display: "inline-block",
 								}}
 							>
 								<img
@@ -161,20 +391,13 @@ const StatementEmail = () => {
 						<div
 							style={{
 								marginTop: "32px",
-								display: "flex",
-								gap: "20px",
-								justifyContent: "center",
-								alignItems: "center",
+								textAlign: "center",
 							}}
 						>
 							<div
 								style={{
-									display: "flex",
-									justifyContent: "center",
-									alignItems: "center",
-									width: "20px",
-									height: "20px",
-									objectFit: "contain",
+									display: "inline-block",
+									marginRight: "12px",
 								}}
 							>
 								<img
@@ -186,13 +409,8 @@ const StatementEmail = () => {
 							</div>
 							<div
 								style={{
-									display: "flex",
-
-									justifyContent: "center",
-									alignItems: "center",
-									width: "20px",
-									height: "20px",
-									objectFit: "contain",
+									display: "inline-block",
+									marginRight: "12px",
 								}}
 							>
 								<img
@@ -204,14 +422,7 @@ const StatementEmail = () => {
 							</div>
 							<div
 								style={{
-									display: "flex",
-
-									justifyContent: "center",
-									alignItems: "center",
-									width: "20px",
-									height: "20px",
-
-									objectFit: "contain",
+									display: "inline-block",
 								}}
 							>
 								<img
